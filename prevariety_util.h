9c3ef879292569ab7e2fda24e10ea39c4ca413b8
Jeff Sommars <sommars1@uic.edu>
2016-10-25 12:48:57 -0500

    Inlining functions

--- prevariety_util.h
+++ prevariety_util.h
@@ -10,7 +10,7 @@ class Cone {
 		NNC_Polyhedron HOPolyhedron;
 		NNC_Polyhedron ClosedPolyhedron;
 		vector<set<int> > ClosedIntersectionIndices;
-		set<int> PolytopesVisited;
+		vector<int> PolytopesVisited;
 };
 
 class Edge {
@@ -36,16 +36,16 @@ class Hull {
 		NNC_Polyhedron CPolyhedron;
 		int AffineDimension;
 		int SpaceDimension;
+		vector<Constraint> Eqs;
 };
 
 //------------------------------------------------------------------------------
-double GetPolyhedralIntersectionTime();
-
-//------------------------------------------------------------------------------
-NNC_Polyhedron IntersectCones(NNC_Polyhedron &ph1, NNC_Polyhedron &ph2);
-
-//------------------------------------------------------------------------------
-Cone IntersectCones(Cone C1, Cone C2);
+inline NNC_Polyhedron IntersectCones(NNC_Polyhedron &ph1, NNC_Polyhedron &ph2) {
+	NNC_Polyhedron ph = ph1;
+	ph.add_constraints(ph2.minimized_constraints());
+	ph.affine_dimension();
+	return ph;
+};
 
 //------------------------------------------------------------------------------
 vector<int> GeneratorToPoint(Generator g);
@@ -78,7 +78,37 @@ int InnerProduct(vector<int> V1, vector<int> V2);
 double DoubleInnerProduct(vector<int> V1, vector<double> V2);
 
 //------------------------------------------------------------------------------
-vector<vector<int> > FindInitialForm(vector<vector<int> > &Points, vector<int> &Vector);
+inline vector<vector<int> > FindInitialForm(vector<vector<int> > &Points, vector<int> &Vector) {
+	/*
+		Computes the initial form of a vector and a set of points.
+	*/
+	if (Points.size() == 0) {
+		return Points;
+	};
+	vector<vector<int> > InitialForm;
+
+	vector<int> *Point;
+	Point = &(Points[0]);
+	InitialForm.push_back(*Point);
+	int MinimalIP = 0;
+	for (size_t i = 0; i != (*Point).size(); i++)
+		MinimalIP += Vector[i] * (*Point)[i];
+
+	for (size_t i = 1; i != Points.size(); i++) {
+		Point = &(Points[i]);
+		int IP = 0;
+		for (size_t j = 0; j != (*Point).size(); j++)
+			IP += Vector[j] * (*Point)[j];
+		if (MinimalIP > IP) {
+			MinimalIP = IP;
+			InitialForm.clear();
+			InitialForm.push_back(*Point);
+		} else if (IP == MinimalIP)
+			InitialForm.push_back(*Point);
+	};
+
+	return InitialForm;
+};
 
 //------------------------------------------------------------------------------
 NNC_Polyhedron FindCPolyhedron(vector<vector<int> > Points);
@@ -99,10 +129,41 @@ void PrintCPolyhedron(NNC_Polyhedron ph, bool PrintIf0Dim = true);
 void PrintCPolyhedrons(vector<NNC_Polyhedron> phs, bool PrintIf0Dim = true);
 
 //------------------------------------------------------------------------------
-set<int> IntersectSets(set<int> S1, set<int> S2);
+inline set<int> IntersectSets(set<int> S1, set<int> S2) {
+	set<int>::iterator S1Itr = S1.begin();
+	set<int>::iterator S2Itr = S2.begin();
+	set<int> Result;
+	while ((S1Itr != S1.end()) && (S2Itr != S2.end())) {
+		if (*S1Itr < *S2Itr) {
+			++S1Itr;
+		}
+		else if (*S2Itr<*S1Itr) {
+			++S2Itr;
+		} else {
+			Result.insert(*S1Itr);
+			S1Itr++;
+			S2Itr++;
+		};
+	};
+	return Result;
+};
 
 //------------------------------------------------------------------------------
-bool SetsDoIntersect(set<int> &S1, set<int> &S2);
+inline bool SetsDoIntersect(set<int> &S1, set<int> &S2) {
+	set<int>::iterator S1Itr = S1.begin();
+	set<int>::iterator S2Itr = S2.begin();
+	while ((S1Itr != S1.end()) && (S2Itr != S2.end())) {
+		if (*S1Itr < *S2Itr) {
+			++S1Itr;
+		}
+		else if (*S2Itr<*S1Itr) {
+			++S2Itr;
+		} else {
+			return true;
+		};
+	};
+	return false;
+};
 
 //------------------------------------------------------------------------------
 set<int> PreintersectWalkPolytope(int HullIndex, Cone NewCone, vector<Hull> &Hulls);
