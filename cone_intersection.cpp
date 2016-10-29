8dc6c272c7e77a609c6dcb8effdc10ddf99fd328
Jeff Sommars <sommars1@uic.edu>
2016-10-28 22:27:01 -0500

    Output prevariety instead of just pretropisms.

--- cone_intersection.cpp
+++ cone_intersection.cpp
@@ -22,7 +22,6 @@ namespace Parma_Polyhedra_Library {using IO_Operators::operator<<;}
 
 double IntersectionTime, TestTime;
 int ConeIntersectionCount;
-mutex BPmtx;
 
 //------------------------------------------------------------------------------
 inline vector<Cone> WalkPolytope(int HullIndex, Cone &NewCone, vector<Hull> &Hulls) {
@@ -130,7 +129,7 @@ inline vector<Cone> WalkPolytope(int HullIndex, Cone &NewCone, vector<Hull> &Hul
 }
 
 //------------------------------------------------------------------------------
-inline vector<Cone> DynamicEnumerate(Cone &C, vector<Hull> &Hulls, vector<vector<int> > &Pretropisms) {
+inline vector<Cone> DynamicEnumerate(Cone &C, vector<Hull> &Hulls) {
 	// Figure out which polytope we want to pick
 	int SmallestInt = 10000000; // Lazy.
 	int SmallestIndex = -1;
@@ -148,139 +147,105 @@ inline vector<Cone> DynamicEnumerate(Cone &C, vector<Hull> &Hulls, vector<vector
 	C.PolytopesVisited[SmallestIndex] = 1;
 	vector<int> PolytopesVisited = C.PolytopesVisited;
 	vector<Cone> ResultCones = WalkPolytope(SmallestIndex, C, Hulls);
-	if (ResultCones.size() == 0) {
-		vector<Cone> Temp;
-		return Temp;
-	};
-	for (size_t i = 0; i != ResultCones.size(); i++) {
-		ResultCones[i].PolytopesVisited = PolytopesVisited;
-	};
-	if (find(ResultCones[0].PolytopesVisited.begin(),ResultCones[0].PolytopesVisited.end(),0) == ResultCones[0].PolytopesVisited.end()) {
-		for (size_t i = 0; i != ResultCones.size(); i++) {
-			Generator_System gs = ResultCones[i].HOPolyhedron.minimized_generators();
-			for (Generator_System::const_iterator gsi = gs.begin(), gs_end = gs.end(); gsi != gs_end; ++gsi) {
-				if ((*gsi).is_point() or (*gsi).is_closure_point()) {
-					continue;
-				};
 
-				// There has to be a better way to do this. This works for now.
-				// Eventually, when switched to closed cones of dimension n+1, this
-				// won't be necessary.
-				Generator_System TempGS;
-				TempGS.insert((*gsi));
-				//
-				Linear_Expression LE;
-				for (size_t j = 0; j < (*gsi).space_dimension(); j++) {
-					LE += Variable(j) * (*gsi).coefficient(Variable(j));
-				};
-				TempGS.insert(point(LE));
-				NNC_Polyhedron RayTestPoly(TempGS);
-				if (not ResultCones[i].HOPolyhedron.contains(RayTestPoly)) {
-					continue;
-				}
-				
-				if ((*gsi).is_ray()) {
-					Pretropisms.push_back(GeneratorToPoint(*gsi));
-				} else if ((*gsi).is_line()) {
-					vector<int> PositivePretropism = GeneratorToPoint(*gsi);
-					Pretropisms.push_back(PositivePretropism);
-					vector<int> NegativePretropism;
-					for (size_t j = 0; j != PositivePretropism.size(); j++) {
-						NegativePretropism.push_back((-1) * PositivePretropism[j]);
-					};
-					Pretropisms.push_back(NegativePretropism);
-				};
-			};
-		};
-		vector<Cone> Temp;
-		return Temp;
-	} else {
-		return ResultCones;
-	};
+	for (size_t i = 0; i != ResultCones.size(); i++)
+		ResultCones[i].PolytopesVisited = PolytopesVisited;
+	return ResultCones;
 };
 
 //------------------------------------------------------------------------------
-void ENUMERATETEST(vector<Hull> Hulls, int ProcessID, int ProcessCount, vector<ThreadJob> &ThreadJobs, vector<int> &BoredProcesses, int &FinishedProcessCount, vector<vector<int> > &GlobalPretropisms) {
-	// Consider holding onto more than one cone. Could hold onto any number.
+void ENUMERATETEST(vector<Hull> Hulls, int ProcessID, int ProcessCount, vector<ThreadJob> &ThreadJobs, vector<int> &BoredProcesses, int &FinishedProcessCount, vector<C_Polyhedron> &PreTropicalPrevariety, mutex &BPmtx) {
 	vector<vector<int> > Pretropisms;
 	Cone C;
 	bool HasCone = false;
 	ThreadJob *TJ;
 	while (true) {
+		int j = ProcessID;
 		while (not HasCone) {
-			int j = ProcessID;
-			while (true) {
-				TJ = &ThreadJobs[j % ProcessCount];
-				(*TJ).M.lock();
-				for (size_t i = (*TJ).SharedCones.size() - 1; i !=-1; i--) {
-					if ((*TJ).SharedCones[i].size() > 0) {
-						C = (*TJ).SharedCones[i].back();
-						(*TJ).SharedCones[i].pop_back();
-						HasCone = true;
-						// This case happens when the process tried to steal from every process
-						// possible but did not succeed, which made it bored. It added itself
-						// to BoredProcesses, but then continued looking and found a cone it
-						// could have. This made it no longer a bored process.
-						if (j > (ProcessID + ProcessCount)) {
-							BPmtx.lock();
-							BoredProcesses[ProcessID] = 0;
-							BoredProcesses[ProcessCount] -= 1;
-							BPmtx.unlock();
-						};
-						break;
+			TJ = &ThreadJobs[j % ProcessCount];
+			(*TJ).M.lock();
+			for (size_t i = (*TJ).SharedCones.size() - 1; i !=-1; i--) {
+				if ((*TJ).SharedCones[i].size() > 0) {
+					C = (*TJ).SharedCones[i].back();
+					(*TJ).SharedCones[i].pop_back();
+					HasCone = true;
+					// This case happens when the process tried to steal from every process
+					// possible but did not succeed, which made it bored. It added itself
+					// to BoredProcesses, but then continued looking and found a cone it
+					// could have. This made it no longer a bored process.
+					if (j > (ProcessID + ProcessCount)) {
+						BPmtx.lock();
+						BoredProcesses[ProcessID] = 0;
+						BoredProcesses[ProcessCount] -= 1;
+						BPmtx.unlock();
 					};
-				};
-				(*TJ).M.unlock();
-				if (HasCone) {
 					break;
 				};
-				// This case means that we spun through all of the other threads' queues
-				// and they were all empty. Now we need to make the process bored.
-				if (j == ProcessCount + ProcessID) {
-					BPmtx.lock();
-					BoredProcesses[ProcessID] = 0;
-					BoredProcesses[ProcessCount] += 1;
-					BPmtx.unlock();
-				};
-				if ((j % ProcessCount) == ProcessID) {
-					if (BoredProcesses[ProcessCount] == ProcessCount) {
-						BPmtx.lock();
-						for (size_t i = 0; i != Pretropisms.size(); i++) {
-							GlobalPretropisms.push_back(Pretropisms[i]);
-						};
-						FinishedProcessCount++;
-						BPmtx.unlock();
-						return;
-					};
-				};
-				j++;
 			};
+			(*TJ).M.unlock();
+			if (HasCone)
+				break;
+			// This case means that we spun through all of the other threads' queues
+			// and they were all empty. Now we need to make the process bored.
+			if (j == ProcessCount + ProcessID) {
+				BPmtx.lock();
+				BoredProcesses[ProcessID] = 0;
+				BoredProcesses[ProcessCount] += 1;
+				BPmtx.unlock();
+			};
+			if ((j % ProcessCount) == ProcessID) {
+				if (BoredProcesses[ProcessCount] == ProcessCount) {
+					BPmtx.lock();
+					FinishedProcessCount++;
+					BPmtx.unlock();
+					return;
+				};
+			};
+			j++;
 		};
-		vector<Cone> ResultCones = DynamicEnumerate(C, Hulls, Pretropisms);
-		// If there is at least one cone, hold onto it for the next round.
-		if (ResultCones.size() > 0) {
-			C = ResultCones.back();
-			ResultCones.pop_back();
-		} else {
-			HasCone = false;
-		};
+		vector<Cone> ResultCones = DynamicEnumerate(C, Hulls);
 		
 		// If there are remaining new cones, give them to the job queue.
 		if (ResultCones.size() > 0) {
 			int Index = -1;
 			for (size_t i = 0; i != ResultCones[0].PolytopesVisited.size(); i++) {
-				if (ResultCones[0].PolytopesVisited[i] == 1) {
+				if (ResultCones[0].PolytopesVisited[i] == 1)
 					Index++;
+			};
+			if (Index == Hulls.size() - 1) {
+				// convert them to C_Polyhedron
+				vector<C_Polyhedron> PrevarietyCones;
+				for (size_t i = 0; i != ResultCones.size(); i++) {
+					Generator_System gs = ResultCones[i].HOPolyhedron.minimized_generators();
+					Generator_System NewGS;
+					NewGS.insert(point(Linear_Expression(0)));
+					for (Generator_System::const_iterator gsi = gs.begin(), gs_end = gs.end(); gsi != gs_end; ++gsi) {
+						if ((*gsi).is_point() or (*gsi).is_closure_point())
+							continue;
+						NewGS.insert(*gsi);
+					};
+					PrevarietyCones.push_back(C_Polyhedron(NewGS));
+				};
+				BPmtx.lock();
+				for (size_t i = 0; i != PrevarietyCones.size(); i++)
+					PreTropicalPrevariety.push_back(PrevarietyCones[i]);
+				BPmtx.unlock();
+				HasCone = false;
+			} else {
+				// If there is at least one cone, hold onto it for the next round.
+				C = ResultCones.back();
+				ResultCones.pop_back();
+				if (ResultCones.size() > 0) {
+					TJ = &ThreadJobs[ProcessID];
+					(*TJ).M.lock();
+					for (size_t i = 0; i != ResultCones.size(); i++) {
+						(*TJ).SharedCones[Index].push_back(ResultCones[i]);
+					};
+					(*TJ).M.unlock();
 				};
 			};
-			
-			TJ = &ThreadJobs[ProcessID];
-			(*TJ).M.lock();
-			for (size_t i = 0; i != ResultCones.size(); i++) {
-				(*TJ).SharedCones[Index].push_back(ResultCones[i]);
-			};
-			(*TJ).M.unlock();
-		};
+		} else
+			HasCone = false;
 	};
 };
 
@@ -317,12 +282,124 @@ int main(int argc, char* argv[]) {
 		double dd = rand();
 		VectorForOrientation.push_back(dd);
 	};
-	for (size_t i = 0; i != PolynomialSystemSupport.size(); i++) {
-		cout << "Hull " << i + 1 << " of " << PolynomialSystemSupport.size() << endl;
+	
+	for (size_t i = 0; i != PolynomialSystemSupport.size(); i++)
 		Hulls.push_back(NewHull(PolynomialSystemSupport[i], VectorForOrientation));
-	};
 	
 	double HullTime = double(clock() - StartTime);
+
+
+
+
+
+
+
+
+
+
+
+/*
+	vector<Cone> ConeVector;
+		// Start by initializing the objects.
+	vector<vector<Cone> > Cones;
+	int HullIndex = 0;
+	vector<Hull>::iterator it;
+	for (it=Hulls.begin(); it != Hulls.end(); it++) {
+		vector<Edge> Edges = (*it).Edges;
+		vector<Cone> HullCones;
+		vector<Edge>::iterator itr;
+
+		for (itr=Edges.begin(); itr != Edges.end(); itr++) {
+			HullCones.push_back((*itr).EdgeCone);
+		};
+	
+		if (HullIndex == 0) {
+			ConeVector = HullCones;
+		} else {
+			Cones.push_back(HullCones);
+		};			
+		HullIndex++;
+	};
+
+	cout << "ConeVector count: " << ConeVector.size() << endl;
+	cout << "Cones count: " << Cones.size() << endl;
+	
+	//Iterate through Cones
+	vector<vector<Cone> >::iterator ConesItr;
+	int TreeLevel = 1;
+	for (ConesItr=Cones.begin(); ConesItr != Cones.end(); ConesItr++) {
+		vector<Cone> TestCones = *ConesItr;
+		vector<Cone> NewCones;
+		//Iterate through Cones
+		vector<Cone>::iterator ConeItr;
+		for (ConeItr=ConeVector.begin(); ConeItr != ConeVector.end(); ConeItr++) {
+			//Iterate through TestCones
+			Cone Cone1 = *ConeItr;
+			vector<Cone>::iterator TestConesItr;
+			for (TestConesItr=TestCones.begin(); TestConesItr != TestCones.end(); TestConesItr++) {
+				Cone NewCone;
+				NewCone.HOPolyhedron = IntersectCones((*TestConesItr).HOPolyhedron, Cone1.HOPolyhedron);
+				ConeIntersectionCount++;
+				if (NewCone.HOPolyhedron.affine_dimension() == 0) {
+					continue;
+				};
+				NewCones.push_back(NewCone);
+			};
+		};
+		ConeVector = NewCones;
+		printf("Finished level %d of tree with %lu levels. %lu cones remain at this level. IntersectionCount = %d.\n", TreeLevel, Cones.size(), ConeVector.size(),ConeIntersectionCount);
+		TreeLevel++;
+	};
+
+
+	vector<vector<int> > PretropismsTemp;
+	for (size_t i = 0; i != ConeVector.size(); i++) {
+		Generator_System gs = ConeVector[i].HOPolyhedron.minimized_generators();
+		cout << ConeVector[i].HOPolyhedron.affine_dimension() << endl;
+		cout << ConeVector[i].HOPolyhedron.minimized_constraints() << endl;
+		cout << gs << endl;
+		cout << "TEST" << endl;
+		for (Generator_System::const_iterator gsi = gs.begin(), gs_end = gs.end(); gsi != gs_end; ++gsi) {
+			if ((*gsi).is_point() or (*gsi).is_closure_point()) {
+				continue;
+			};
+			vector<int> Pretrop = GeneratorToPoint(*gsi);
+			if (find(PretropismsTemp.begin(), PretropismsTemp.end(), Pretrop) == PretropismsTemp.end() ) {
+				PretropismsTemp.push_back(Pretrop);
+			};
+		};
+	};
+	sort(PretropismsTemp.begin(), PretropismsTemp.end());
+
+	PrintPoints(PretropismsTemp);
+	cout << "Number of pretropisms found: " << PretropismsTemp.size() << endl;
+	cin.get();
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	double PreintersectTime = 0;
 
@@ -390,7 +467,7 @@ int main(int argc, char* argv[]) {
 		};
 	};
 	if (SmallestIndex == -1) {
-		cout << "Internal error: DynamicEnumerate had a value of -1 for SmallestIndex" << endl;
+		cout << "Internal error: value of -1 for SmallestIndex" << endl;
 		return 1;
 	};
 	
@@ -419,6 +496,8 @@ int main(int argc, char* argv[]) {
 		ThreadJobs[i % TotalProcessCount].SharedCones[0].push_back(Hulls[SmallestIndex].Edges[i].EdgeCone);
 	};
 	
+	mutex BPmtx;
+	vector<C_Polyhedron> PreTropicalPrevariety;
 	vector<vector<int> > GlobalPretropisms;
 	vector<int> BoredProcesses (TotalProcessCount + 1, 0);
 	int FinishedProcessCount = 0;
@@ -427,7 +506,7 @@ int main(int argc, char* argv[]) {
 	Thread_Pool<work_type> thread_pool(TotalProcessCount);
 	// Submit all conversion tasks.
 	for (size_t i = 0; i != TotalProcessCount; i++) {
-		work_type work = bind(ENUMERATETEST, Hulls, i, TotalProcessCount, ref(ThreadJobs), ref(BoredProcesses), ref(FinishedProcessCount), ref(GlobalPretropisms));
+		work_type work = bind(ENUMERATETEST, Hulls, i, TotalProcessCount, ref(ThreadJobs), ref(BoredProcesses), ref(FinishedProcessCount), ref(PreTropicalPrevariety), ref(BPmtx));
 		thread_pool.submit(make_threadable(work));
 	}
 	// Wait for all workers to complete.
@@ -439,10 +518,56 @@ int main(int argc, char* argv[]) {
 			this_thread::sleep_for(chrono::milliseconds(100));
 		};
 	};
+	clock_t CleanupStart = clock();
+	cout << "Starting cleanup: " << double(clock() - AlgorithmStartTime) / CLOCKS_PER_SEC << endl;
+	vector<C_Polyhedron> TropicalPrevariety;
+	vector<int> SupersededCones (PreTropicalPrevariety.size(), 0);
+	C_Polyhedron *C1;
+	vector<int> MaximalConeCount;
+	for (size_t i = 0; i != PreTropicalPrevariety.size(); i++) {
+		bool AddCone = true;
+		C1 = &(PreTropicalPrevariety[i]);
+		for (size_t j = 0; j != PreTropicalPrevariety.size(); j++) {
+			if ((i == j) 
+			|| (SupersededCones[i] == 1)
+			|| ((*C1).affine_dimension() > PreTropicalPrevariety[j].affine_dimension()))
+				continue;
+			if (PreTropicalPrevariety[j].contains(*C1)) {
+				AddCone = false;
+				SupersededCones[i] = 1;
+				break;
+			};
+		};
+		if (AddCone == true) {
+			TropicalPrevariety.push_back(*C1);
+			for (Generator_System::const_iterator g = 
+			(*C1).minimized_generators().begin(), gs_end = 
+			(*C1).minimized_generators().end(); g != gs_end; ++g) {
+				if ((*g).is_point()) 
+					continue;
+				vector<int> Pretropism = GeneratorToPoint(*g);
+				if (find(GlobalPretropisms.begin(), GlobalPretropisms.end(), Pretropism) == GlobalPretropisms.end())
+					GlobalPretropisms.push_back(Pretropism);
+			};
+			int Dim = (*C1).affine_dimension();
+			while (Dim > MaximalConeCount.size()) {
+				MaximalConeCount.push_back(0);
+			};
+			MaximalConeCount[Dim - 1] += 1;
+		};
+	};
+	for (size_t i = 0; i != TropicalPrevariety.size(); i++) {
+		cout << TropicalPrevariety[i].affine_dimension() << endl;
+		cout << TropicalPrevariety[i].minimized_generators() << endl;
+	};
+	
 	double TimeAfterInit = double(clock() - AlgorithmStartTime);
 	sort(GlobalPretropisms.begin(), GlobalPretropisms.end());
 	PrintPoints(GlobalPretropisms);
+	cout << "Maximal cone count--------" << endl;
+	PrintPoint(MaximalConeCount);
 	cout << "Number of pretropisms found: " << GlobalPretropisms.size() << endl;
+	cout << "Cleanup time: " << double(clock() - CleanupStart) / CLOCKS_PER_SEC << endl;
 	cout << "Hull time: " << HullTime / CLOCKS_PER_SEC << endl;
 	cout << "Intersection time: " << IntersectionTime / CLOCKS_PER_SEC << endl;
 	cout << "Preintersection time: " << PreintersectTime / CLOCKS_PER_SEC << endl;
