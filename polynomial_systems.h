c4e020d72baf34914da941ce45978a1250cb3f36
Jeff Sommars <sommars1@uic.edu>
2017-02-20 11:27:13 -0600

    Update polynomial_systems.h

--- polynomial_systems.h
+++ polynomial_systems.h
@@ -1,8 +1,10 @@
 #include <ppl.hh>
 #include <boost/dynamic_bitset.hpp>
 #include "Thread_Pool_defs.hh"
+#include "/home/jeff/Desktop/Software/soplex-2.2.1/src/soplex.h"
 
 using namespace std;
+using namespace soplex;
 namespace Parma_Polyhedra_Library {using IO_Operators::operator<<;}
 
 //------------------------------------------------------------------------------
